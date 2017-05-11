54703e6c96d9434a6e71a7bf64b079e793eabf1e
pankona <yosuke.akatsuka@gmail.com>
2017-05-11 12:29:48 +0900

    remove unnecessary title background images.
    just show scene name by text

--- scene/title.go
+++ scene/title.go
@@ -10,8 +10,7 @@ import (
 
 // Title represents a scene object for Title
 type Title struct {
-	background simra.Sprite
-	text       simra.Sprite
+	text simra.Sprite
 }
 
 // Initialize initializes title scene
@@ -30,29 +29,17 @@ func (title *Title) Initialize() {
 }
 
 func (title *Title) initialize() {
-	// add background sprite
-	title.background.W = config.ScreenWidth
-	title.background.H = config.ScreenHeight
-
-	// put center of screen
-	title.background.X = config.ScreenWidth / 2
-	title.background.Y = config.ScreenHeight / 2
-
-	simra.GetInstance().AddSprite("title.png",
-		image.Rect(0, 0, 960, 540),
-		&title.background)
-
-	title.text.W = 320
+	title.text.W = config.ScreenWidth
 	title.text.H = 80
-	title.text.X = title.text.W / 2
-	title.text.Y = title.text.H / 2
-	simra.GetInstance().AddTextSprite("text sample",
+	title.text.X = config.ScreenWidth / 2
+	title.text.Y = config.ScreenHeight / 2
+	simra.GetInstance().AddTextSprite("title",
 		60, // fontsize
 		color.RGBA{255, 0, 0, 255},
 		image.Rect(0, 0, int(title.text.W), int(title.text.H)),
 		&title.text)
 
-	title.background.AddTouchListener(title)
+	simra.GetInstance().AddTouchListener(title)
 }
 
 // Drive is called from simra.
@@ -62,17 +49,14 @@ func (title *Title) Drive() {
 }
 
 // OnTouchBegin is called when Title scene is Touched.
-// It is caused by calling AddtouchListener for title.background sprite.
 func (title *Title) OnTouchBegin(x, y float32) {
 }
 
 // OnTouchMove is called when Title scene is Touched and moved.
-// It is caused by calling AddtouchListener for title.background sprite.
 func (title *Title) OnTouchMove(x, y float32) {
 }
 
 // OnTouchEnd is called when Title scene is Touched and it is released.
-// It is caused by calling AddtouchListener for title.background sprite.
 func (title *Title) OnTouchEnd(x, y float32) {
 	// scene end. go to next scene
 	simra.GetInstance().SetScene(&Stage1{})
