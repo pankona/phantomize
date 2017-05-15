a2e51713f2f983f9561f9ffa7b9d60fff1729c31
pankona <yosuke.akatsuka@gmail.com>
2017-05-15 21:45:02 +0900

    show ctrl panel on game scene

--- scene/game.go
+++ scene/game.go
@@ -14,6 +14,7 @@ type game struct {
 	text         simra.Sprite
 	currentStage int
 	nextScene    simra.Driver
+	ctrlPanel    simra.Sprite
 }
 
 // Initialize initializes game scene
@@ -31,11 +32,7 @@ func (game *game) Initialize() {
 	simra.LogDebug("[OUT]")
 }
 
-func (game *game) initialize() {
-	initTextSprite(&game.text, "game for stage "+strconv.Itoa(game.currentStage),
-		config.ScreenWidth, 80, config.ScreenWidth/2, config.ScreenHeight*4/6,
-		60, color.RGBA{255, 0, 0, 255})
-
+func (game *game) initTempText() {
 	// temporary text (will be removed)
 	temporary := &simra.Sprite{}
 	temporary.W = config.ScreenWidth
@@ -47,7 +44,25 @@ func (game *game) initialize() {
 		color.RGBA{255, 0, 0, 255},
 		image.Rect(0, 0, int(temporary.W), int(temporary.H)),
 		temporary)
+}
 
+func (game *game) initCtrlPanel() {
+	game.ctrlPanel.W = config.ScreenWidth
+	game.ctrlPanel.H = 240
+	game.ctrlPanel.X = config.ScreenWidth / 2
+	game.ctrlPanel.Y = 120
+	simra.GetInstance().AddSprite("ctrl_panel.png",
+		image.Rect(0, 0, 1280, 240),
+		&game.ctrlPanel)
+}
+
+func (game *game) initialize() {
+	initTextSprite(&game.text, "game for stage "+strconv.Itoa(game.currentStage),
+		config.ScreenWidth, 80, config.ScreenWidth/2, config.ScreenHeight*4/6,
+		60, color.RGBA{255, 0, 0, 255})
+	// temporary text (will be removed)
+	game.initTempText()
+	game.initCtrlPanel()
 	simra.GetInstance().AddTouchListener(game)
 }
 
