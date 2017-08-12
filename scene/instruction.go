a7fdda4d426e3cc3a05da2d46cf106d574b7728d
pankona <yosuke.akatsuka@gmail.com>
2017-08-13 01:10:26 +0900

    don't create new text texture every time in instruction.go

--- scene/instruction.go
+++ scene/instruction.go
@@ -11,35 +11,37 @@ import (
 type instruction struct {
 	sprite *simra.Sprite
 	game   *game
+	texs   [2]*simra.Texture
 }
 
 func (in *instruction) initialize() {
 	in.sprite = simra.NewSprite()
 	simra.GetInstance().AddSprite2(in.sprite)
+	in.texs[0] = simra.NewTextTexture(
+		"choose first unit from here ↓",
+		40, // fontsize
+		color.RGBA{255, 255, 255, 255},
+		image.Rect(0, 0, config.ScreenWidth, 80),
+	)
+
+	in.texs[1] = simra.NewTextTexture(
+		"↑ tap field to summon the unit",
+		40, // fontsize
+		color.RGBA{255, 255, 255, 255},
+		image.Rect(0, 0, config.ScreenWidth, 80),
+	)
 }
 
 func (in *instruction) OnEvent(i interface{}) {
 	c := i.(*command)
 	switch c.commandtype {
 	case commandGameStarted:
-		tex := simra.NewTextTexture(
-			"choose first unit from here ↓",
-			40, // fontsize
-			color.RGBA{255, 255, 255, 255},
-			image.Rect(0, 0, config.ScreenWidth, 80),
-		)
-		in.sprite.ReplaceTexture2(tex)
+		in.sprite.ReplaceTexture2(in.texs[0])
 		in.sprite.X, in.sprite.Y = config.ScreenWidth/2+240, 260
 		in.sprite.W, in.sprite.H = config.ScreenWidth, 80
 
 	case commandUpdateSelection:
-		tex := simra.NewTextTexture(
-			"↑ tap field to summon the unit",
-			40, // fontsize
-			color.RGBA{255, 255, 255, 255},
-			image.Rect(0, 0, config.ScreenWidth, 80),
-		)
-		in.sprite.ReplaceTexture2(tex)
+		in.sprite.ReplaceTexture2(in.texs[1])
 		in.sprite.X, in.sprite.Y = config.ScreenWidth/2+275, 260
 		in.sprite.W, in.sprite.H = config.ScreenWidth, 80
 
